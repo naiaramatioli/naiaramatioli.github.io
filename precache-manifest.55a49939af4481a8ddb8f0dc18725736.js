5a1c7454ef83021965deaee9d05331ef82e2b13f
Naiara Santana Matioli <naiara.santana1992@gmail.com>
2020-07-14 03:42:17 -0300

    Ivy is publishing on master because shes evil

---
+++ precache-manifest.55a49939af4481a8ddb8f0dc18725736.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31146c99b57d2fe56c8d61eac481e06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0eca6ad06a2e4fffb935",
+    "url": "/static/js/2.1473b47b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1473b47b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fa135c2adbc4d91a12b",
+    "url": "/static/js/main.a0434326.chunk.js"
+  },
+  {
+    "revision": "54bbfad2b486d03be139",
+    "url": "/static/js/runtime-main.fc8ff760.js"
+  }
+]);
